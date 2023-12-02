a98a687cb1915b1dc8c9343fae7349361e5f84a8
jaibtdou <iamnoob2210@gmail.com>
2023-12-02 16:27:52 +0700

    New data 12/2/2023

---
+++ py/merger.py
@@ -0,0 +1,23 @@
+import requests
+import pandas as pd
+def get_futures_symbols():
+    url = "https://fapi.binance.com/fapi/v1/exchangeInfo"
+    data = requests.get(url).json()
+    symbols = []
+    for symbol in data['symbols']:
+        symbols.append(symbol['symbol'])
+    return symbols
+
+futures_symbols = get_futures_symbols()
+
+for symbol in futures_symbols:
+    df = pd.read_csv('data/long_short_ratio/5m/' + symbol + '.csv')
+    df1 = pd.read_csv('Trivix/data/long_short_ratio/5m/' + symbol + '.csv')
+    # Merge 2 file together
+    # If there are rows from 2 files that have the same timestamp, keep the row from the first file
+    # Otherwise add both data
+    df = pd.concat([df, df1], ignore_index=True)
+    if (symbol == "1000LUNCUSDT"):
+        print(df)
+    df = df.drop_duplicates(subset=['Timestamp'], keep='first')
+    df.to_csv('data/long_short_ratio/5m/' + symbol + '.csv', index=False)
