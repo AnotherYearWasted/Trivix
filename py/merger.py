2aa3862bea5e0125d1a421117b28a78858e07653
jaibtdou <iamnoob2210@gmail.com>
2023-12-02 16:51:48 +0700

    Fixed data 12/2/2023

--- py/merger.py
+++ py/merger.py
@@ -12,11 +12,16 @@ futures_symbols = get_futures_symbols()
 
 for symbol in futures_symbols:
     df = pd.read_csv('data/long_short_ratio/5m/' + symbol + '.csv')
-    df1 = pd.read_csv('Trivix/data/long_short_ratio/5m/' + symbol + '.csv')
+    try:
+        df1 = pd.read_csv('Trivix/data/long_short_ratio/5m/' + symbol + '.csv')
+    except:
+        df1 = df
     # Merge 2 file together
     # If there are rows from 2 files that have the same timestamp, keep the row from the first file
     # Otherwise add both data
     df = pd.concat([df, df1], ignore_index=True)
+    # Sort by timestamp
+    df = df.sort_values(by=['Timestamp'])
     if (symbol == "1000LUNCUSDT"):
         print(df)
     df = df.drop_duplicates(subset=['Timestamp'], keep='first')
