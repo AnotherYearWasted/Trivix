c196fc6fe61994a7eefef9d660fe32d542aebc28
jaibtdou <iamnoob2210@gmail.com>
2023-12-20 23:52:01 +0700

    Updated code to get data

--- py/getdata.py
+++ py/getdata.py
@@ -1,20 +1,24 @@
-import requests
+import httpx
 import json
 import pandas as pd
 import zipfile
 import datetime, io, os
 from typing import Literal
+import asyncio
+from market import futures_exchange_information
+
+client = httpx.AsyncClient(timeout=None)
 
 # url may look like this
 # https://data.binance.vision/data/futures/um/daily/markPriceKlines/BTCUSDT/1m/BTCUSDT-1m-2023-12-12.zip
 # Get data daily from the last 30 days
-def get_klines(symbol: str, 
+async def get_klines(symbol: str, 
              date: str, 
              func: Literal["klines", "markPriceKlines", "premiumIndexKlines", "indexPriceKlines"]
              ) -> pd.DataFrame:
     url = f'https://data.binance.vision/data/futures/um/daily/{func}/{symbol}/1m/' + symbol + '-1m-' + date + '.zip'
     """download content from a url, unzip, and save to a csv file"""
-    r = requests.get(url)
+    r = await client.get(url)
     print(url)
     z = zipfile.ZipFile(io.BytesIO(r.content))
     z.extractall(f'data/{func}/')
@@ -25,38 +29,55 @@ def get_klines(symbol: str,
     return df
 
 # https://data.binance.vision/data/futures/um/daily/metrics/1000BONKUSDT/1000BONKUSDT-metrics-2023-12-12.zip
-def get_metrics(symbol: str,
+async def get_metrics(symbol: str,
                 date: str,
                 func: Literal["metrics", "liquidationSnapshot", "trades", "aggTrades","bookDepth", "bookTicker"]
                 ) -> pd.DataFrame:
-    url = f'https://data.binance.vision/data/futures/um/daily/{func}/{symbol}/' + symbol + '-' + func + '-' + date + '.zip'
-    """download content from a url, unzip, and save to a csv file"""
-    r = requests.get(url)
-    print(url)
-    z = zipfile.ZipFile(io.BytesIO(r.content))
-    z.extractall(f'data/{func}/')
-    df = pd.read_csv(f'data/{func}/' + symbol + '-' + func + '-' + date + '.csv')
-    df.to_csv(f'data/{func}/' + symbol + '-' + func + '-' + date + '.csv')
-    # Delete the csv file
-    os.remove(f'data/{func}/' + symbol + '-' + func + '-' + date + '.csv')
+    try:
+        url = f'https://data.binance.vision/data/futures/um/daily/{func}/{symbol}/' + symbol + '-' + func + '-' + date + '.zip'
+        """download content from a url, unzip, and save to a csv file"""
+        r = await client.get(url)
+        print(url)
+        z = zipfile.ZipFile(io.BytesIO(r.content))
+        z.extractall(f'data/{func}/')
+        df = pd.read_csv(f'data/{func}/' + symbol + '-' + func + '-' + date + '.csv')
+        df.to_csv(f'data/{func}/' + symbol + '-' + func + '-' + date + '.csv')
+        # Delete the csv file
+        os.remove(f'data/{func}/' + symbol + '-' + func + '-' + date + '.csv')
+    except:
+        df = pd.DataFrame()
     return df
 
-def get_daily_data():
+
+
+async def get_daily_data_single(symbol: str, func: str, days: int):
     """get daily data from the last 30 days"""
     today = datetime.datetime.today()
-    symbol = 'BTCUSDT'
-    func = 'klines'
+    symbol = symbol
+    func = func
     df = pd.DataFrame()
-    for i in range(2, 366):
+    tasks = []
+    for i in range(2, 2 + days):
         date = today - datetime.timedelta(days=i)
         date = date.strftime('%Y-%m-%d')
-        df1 = get_klines(symbol, date, func)
-        # Concatenate dataframes
-        df = pd.concat([df, df1])
+        tasks.append(asyncio.create_task(get_klines(symbol, date, func)))
+    
+    # Wait for all tasks to complete
+    responses = await asyncio.gather(*tasks)
     # Sort the data by the first column
-    print(df)
+    for response in responses:
+        df = pd.concat([df, response])
     df.sort_values(by=df.columns[0], inplace=True)
+    print(df)
     # Write data to csv file
     df.to_csv(f'data/{func}/' + symbol + '.csv', index=False)
 
-get_daily_data()
+async def get_daily_data(symbols):
+    tasks = []
+    for symbol in symbols:
+        tasks.append(asyncio.create_task(get_daily_data_single(symbol, "klines", 30)))
+    await asyncio.gather(*tasks)
+
+if __name__ == "__main__":
+    symbols = futures_exchange_information("SYMBOL")
+    asyncio.run(get_daily_data(symbols))
